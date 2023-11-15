1762752855571650a39809db97e11467a9f30c2f
Paras Soni <142609712+paras-soni@users.noreply.github.com>
2023-11-15 22:23:17 +0530

    segmentation and beautifying changes

--- netflix_clone/script.js
+++ netflix_clone/script.js
@@ -1,16 +1,14 @@
 // api key from TMDB
-
 const api = "api_key=4ec590464744195701a56f9d3ac717a1";
+
 
 // base url of the site
 const base_url = "https://api.themoviedb.org/3";
-
 const banner_url = "https://image.tmdb.org/t/p/original";
 const img_url = "https://image.tmdb.org/t/p/w500"; //you can change "w185" to your preference
 
 
 // requests for movies data
-
 const requests = {
 
     fetchTopRated: `${base_url}/trending/all/week?${api}&language=en-US`,
@@ -24,31 +22,25 @@ const requests = {
     fetchDocumentaries: `${base_url}/discover/movie?${api}&with_genres=99`,
     fetchCrimeSuspense: `${base_url}/discover/movie?${api}&with_genres=80`
 };
-//used to truncate the string
 
+//used to truncate the string
 function truncate(str, n){
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
 }
-//Banner Movies 
 
+
+//Banner Movies 
 fetch(requests.fetchNetflixOriginals)
     .then((res) => res.json())
-
-
-
     .then((data) => {
 
         //after every refresh , movie will change
-
         const setMovie = 
             data.results[Math.floor(Math.random() * data.results.length - 1)];
-
-
 
             var banner = document.getElementById("banner");
             var bannner_title = document.getElementById("banner__title");
             var banner__desc = document.getElementById("banner__description");
-
 
             banner.style.backgroundImage = 
                 "url(" + banner_url + setMovie.backdrop_path + ")";
@@ -60,8 +52,6 @@ fetch(requests.fetchNetflixOriginals)
     //Top rated 
         fetch(requests.fetchTopRated)
             .then((res) => res.json())
-
-
             .then((data) => {
                 const headrow = document.getElementById("headrow");
                 const row = document.createElement("div");
@@ -69,15 +59,15 @@ fetch(requests.fetchNetflixOriginals)
 
                 headrow.appendChild(row);
 
-
                 const title = document.createElement("h2");
                 title.className = "row__title";
                 title.innerText = " Top rated";
-                row.appendChild(title);
 
+                row.appendChild(title);
 
                 const row_posters = document.createElement("div");
                 row_posters.className = "row__posters";
+
                 row.appendChild(row_posters);
 
 
@@ -93,51 +83,54 @@ fetch(requests.fetchNetflixOriginals)
                 });
             });
 
+
     //Comedy Movies
         fetch(requests.fetchComedyMovies)
             .then((res) => res.json())
-
             .then((data) => {
                 const headrow = document.getElementById("headrow");
                 const row = document.createElement("div");
                 row.className = "row";
-                headrow.appendChild(row);
 
+                headrow.appendChild(row);
 
                 const title = document.createElement("h2");
                 title.className = "row__title";
                 title.innerText = "Comedy Movies";
-                row.appendChild(title);
 
+                row.appendChild(title);
 
                 const row_posters = document.createElement("div");
                 row_posters.className = "row__posters";
                 row.appendChild(row_posters);
-
 
                 data.results.forEach((movie) => {
                     console.log(movie);
                     const poster = document.createElement("img");
                     poster.className = "row__poster";
                     var s2 = movie.id;
+
                     poster.id = s2;
                     poster.src = img_url + movie.backdrop_path;
                     row_posters.appendChild(poster);
                 });
             });
+
+
     //Horror Movies
         fetch(requests.fetchHorrorMovies)
             .then((res) => res.json())
-
             .then((data) => {
                 const headrow = document.getElementById("headrow");
                 const row = document.createElement("div");
                 row.className = "row";
+
                 headrow.appendChild(row);
 
                 const title = document.createElement("h2");
                 title.className = "row__title";
                 title.innerText = "Horror Movies";
+
                 row.appendChild(title);
 
                 const row_posters = document.createElement("div");
@@ -149,24 +142,28 @@ fetch(requests.fetchNetflixOriginals)
                     const poster = document.createElement("img");
                     poster.className = "row__poster";
                     var s2 = movie.id;
+
                     poster.id = s2;
                     poster.src = img_url + movie.backdrop_path;
                     row_posters.appendChild(poster);
                 });
             });
+
+
     //Romance Movies
         fetch(requests.fetchRomanceMovies)
             .then((res) => res.json())
-
             .then((data) => {
                 const headrow = document.getElementById("headrow");
                 const row = document.createElement("div");
                 row.className = "row";
+
                 headrow.appendChild(row);
 
                 const title = document.createElement("h2");
                 title.className = "row__title";
                 title.innerText = "Romance Movies";
+
                 row.appendChild(title);
 
                 const row_posters = document.createElement("div");
@@ -178,25 +175,28 @@ fetch(requests.fetchNetflixOriginals)
                     const poster = document.createElement("img");
                     poster.className = "row__poster";
                     var s2 = movie.id;
+
                     poster.id = s2;
                     poster.src = img_url + movie.backdrop_path;
                     row_posters.appendChild(poster);
                 });
             });
 
+
     //Thriller Movies
         fetch(requests.fetchThrillerMovies)
         .then((res) => res.json())
-
         .then((data) => {
             const headrow = document.getElementById("headrow");
             const row = document.createElement("div");
             row.className = "row";
+
             headrow.appendChild(row);
 
             const title = document.createElement("h2");
             title.className = "row__title";
             title.innerText = "Thriller Movies";
+
             row.appendChild(title);
 
             const row_posters = document.createElement("div");
@@ -208,25 +208,28 @@ fetch(requests.fetchNetflixOriginals)
                 const  poster = document.createElement("img");
                 poster.className = "row__poster";
                 var s2 = movie.id;
+
                 poster.id = s2;
                 poster.src = img_url + movie.backdrop_path;
                 row_posters.appendChild(poster);
             });
         });
 
+
     //Science Fiction Movies
         fetch(requests.fetchSciFiMovies)
             .then((res) => res.json())
-
             .then((data) => {
                 const headrow = document.getElementById("headrow");
                 const row = document.createElement("div");
                 row.className = "row";
+
                 headrow.appendChild(row);
 
                 const title = document.createElement("h2");
                 title.className = "row__title";
                 title.innerText = "Science Fiction";
+
                 row.appendChild(title);
 
                 const row_posters = document.createElement("div");
@@ -238,11 +241,13 @@ fetch(requests.fetchNetflixOriginals)
                     const poster = document.createElement("img");
                     poster.className = "row__poster";
                     var s2 = movie.id;
+
                     poster.id = s2;
                     poster.src = img_url + movie.backdrop_path;
                     row_posters.appendChild(poster);
                 });
             });
+
 
     //kids and family 
         fetch(requests.fetchKidsFamily)
@@ -252,11 +257,13 @@ fetch(requests.fetchNetflixOriginals)
             const headrow = document.getElementById("headrow");
             const row = document.createElement("div");
             row.className = "row";
+
             headrow.appendChild(row);
 
             const title = document.createElement("h2");
             title.className = "row__title";
             title.innerText = "Kids and Family";
+
             row.appendChild(title);
 
             const row_posters = document.createElement("div");
@@ -268,6 +275,7 @@ fetch(requests.fetchNetflixOriginals)
                 const poster = document.createElement("img");
                 poster.className = "row__poster";
                 var s2 = movie.id;
+
                 poster.id = s2;
                 poster.src = img_url + movie.backdrop_path;
                 row_posters.appendChild(poster);
@@ -278,16 +286,17 @@ fetch(requests.fetchNetflixOriginals)
     //Documentaries
         fetch(requests.fetchDocumentaries)
             .then((res) => res.json())
-
             .then((data) => {
                 const headrow = document.getElementById("headrow");
                 const row = document.createElement("div");
                 row.className = "row";
+
                 headrow.appendChild(row);
 
                 const title = document.createElement("h2");
                 title.className = "row__title";
                 title.innerText = "Documentaries";
+
                 row.appendChild(title);
 
                 const row_posters = document.createElement("div");
@@ -299,25 +308,28 @@ fetch(requests.fetchNetflixOriginals)
                     const poster = document.createElement("img");
                     poster.className = "row__poster";
                     var s2 = movie.id;
+
                     poster.id = s2;
                     poster.src = img_url + movie.backdrop_path;
                     row_posters.appendChild(poster);
                 });
             });
 
+
     //Horror Movies
         fetch(requests.fetchCrimeSuspense)
         .then((res) => res.json())
-
         .then((data) => {
             const headrow = document.getElementById("headrow");
             const row = document.createElement("div");
             row.className = "row";
+
             headrow.appendChild(row);
 
             const title = document.createElement("h2");
             title.className = "row__title";
             title.innerText = "Crime and Suspense";
+
             row.appendChild(title);
 
             const row_posters = document.createElement("div");
@@ -329,6 +341,7 @@ fetch(requests.fetchNetflixOriginals)
                 const poster = document.createElement("img");
                 poster.className = "row__poster";
                 var s2 = movie.id;
+
                 poster.id = s2;
                 poster.src = img_url + movie.backdrop_path;
                 row_posters.appendChild(poster);
